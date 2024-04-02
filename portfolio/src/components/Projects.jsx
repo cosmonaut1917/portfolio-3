160c454d7c1ac80ce40316ea6fef25faefbff27f
James <jamesdwire66@gmail.com>
2024-04-02 16:57:58 -0600

    added misc componets

--- portfolio/src/components/Projects.jsx
+++ portfolio/src/components/Projects.jsx
@@ -3,21 +3,20 @@ import Card from 'react-bootstrap/Card';
 import { Link } from 'react-router-dom';
 
 function BasicExample() {
-  return (
-    <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src="holder.js/100px180" />
-      <Card.Body>
-        <Card.Title>Password generator</Card.Title>
-        <Card.Text>
-         You can use this app to generate a password
-        </Card.Text>
-        <Button variant="primary"  >Check it out
-        <Card.Link href="https://github.com/cosmonaut1917?tab=repositories"></Card.Link></Button>
-        
-      </Card.Body>
-    </Card>
-    
-  );
+    return (
+        <Card style={{ width: '18rem' }}>
+            <Card.Img variant="top" src="holder.js/100px180" />
+            <Card.Body>
+                <Card.Title>Password generator</Card.Title>
+                <Card.Text>
+                    You can use this app to generate a password
+                </Card.Text>
+                <Button variant="primary" href='https://github.com/cosmonaut1917' target='blank' >Check it out
+                </Button>
+            </Card.Body>
+        </Card>
+
+    );
 }
 
 export default BasicExample;
