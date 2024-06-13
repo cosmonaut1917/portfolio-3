68fd7771819b29b0153237db563aa0e145023732
James <jamesdwire66@gmail.com>
2024-06-12 20:35:15 -0600

    fixed bug

--- portfolio/src/pages/AboutMe.jsx
+++ portfolio/src/pages/AboutMe.jsx
@@ -7,7 +7,7 @@ function AboutMe (){
         <div>  
         <img style={{width: "18em"}} src={profilePic} alt="profile picture" />   {/* pfp above */}
         
-        <p style={{color:"white"}}>Iam a programmer from Utah. Iam just starting my Programming career and am glad you've decided to checkout my portfolio. 
+        <p style={{color:"white"}}>Iam a programmer from Utah. Iam just starting my Programming career and am glad youve decided to checkout my portfolio. 
             Feel free to look around and check out my projects. Iam always looking for new projects to work on and new people to work with.
         </p>{/*main paragraph for about me  */}
         </div>
