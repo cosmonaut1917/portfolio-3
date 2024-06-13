541669a9fbb78bc1bfae0bc515bfbe5f5046f364
James <jamesdwire66@gmail.com>
2024-06-12 20:29:59 -0600

    fixed pfp and added white text

--- portfolio/src/pages/AboutMe.jsx
+++ portfolio/src/pages/AboutMe.jsx
@@ -5,10 +5,10 @@ function AboutMe (){
         <div style={{backgroundColor:"#1a1a1a",width:"90%",margin:"0 auto",height:"80%",borderRadius:"10px"}}>
         <h2 style={{color:"white"}}>About Me</h2>
         <div>  
-        {/* pfp above */}
-                
-        <p style={{color: "white"}}>I am a programmer from Utah. I am just starting my Programming career and am glad you've decided to checkout my portfolio. 
-            Feel free to look around and check out my projects. I am always looking for new projects to work on and new people to work with.
+        <img style={{width: "18em"}} src={profilePic} alt="profile picture" />   {/* pfp above */}
+        
+        <p style={{color:"white"}}>Iam a programmer from Utah. Iam just starting my Programming career and am glad youve decided to checkout my portfolio. 
+            Feel free to look around and check out my projects. Iam always looking for new projects to work on and new people to work with.
         </p>{/*main paragraph for about me  */}
         </div>
         </div>
