8cbbec393de2de394c20c63c48549b24f3f06be3
James <jamesdwire66@gmail.com>
2024-06-12 20:27:41 -0600

    fixed text color

--- portfolio/src/pages/AboutMe.jsx
+++ portfolio/src/pages/AboutMe.jsx
@@ -1,11 +1,15 @@
 import profilePic from "../assets/Snapchat-421019764.jpg";
 function AboutMe (){
     return (
+
         <div style={{backgroundColor:"#1a1a1a",width:"90%",margin:"0 auto",height:"80%",borderRadius:"10px"}}>
         <h2 style={{color:"white"}}>About Me</h2>
-        <div>
-        <img style={{width: "18em"}} src={profilePic} alt="profile picture" />
-        <p>Iam a programmer from Utah more will be added at a later date</p>
+        <div>  
+        {/* pfp above */}
+                
+        <p style={{color: "white"}}>I am a programmer from Utah. I am just starting my Programming career and am glad you've decided to checkout my portfolio. 
+            Feel free to look around and check out my projects. I am always looking for new projects to work on and new people to work with.
+        </p>{/*main paragraph for about me  */}
         </div>
         </div>
     );
